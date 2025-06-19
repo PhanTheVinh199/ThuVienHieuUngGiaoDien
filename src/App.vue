983d533899c20cd41002d023830a451d4eede2a6
PhanTheVinh199 <phanthevinh199@gmail.com>
2025-06-19 18:15:41 +0700

    xăp sếp dư liệu

--- src/App.vue
+++ src/App.vue
@@ -86,7 +86,7 @@ function toggleSubmenu() {
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap');
 
-html, body {
+ body {
   margin: 0;
   padding: 0;
   font-family: 'Segoe UI', sans-serif;
